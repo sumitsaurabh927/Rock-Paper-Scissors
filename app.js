f5e16ec6e4e5cd2d2c2fa9566869cb784e0e1558
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-04-06 14:21:21 +0530

    coded up entire game

--- app.js
+++ app.js
@@ -1,1 +1,58 @@
-console.log('wired');
+// console.log('wired');
+
+let choicesArr = ["rock", "paper", "scissors"];
+
+// computer's selection function
+const computerPlay = () => {
+  console.log(choicesArr[Math.floor(Math.random() * 3)]);
+  return choicesArr[Math.floor(Math.random() * 3)];
+};
+
+
+// who wins?
+const whoWon = (userPlay, computerSelection) => {
+  // user's winning conditions
+  if (
+    (userPlay === "rock" && computerSelection === "scissors") ||
+    (userPlay === "paper" && computerSelection === "rock") ||
+    (userPlay === "scissors" && computerSelection === "paper")
+  ) {
+    // console.log("User WINS!!!!");
+    return "user";
+  } else if (userPlay === computerSelection) {
+    //   draw conditions
+    // console.log("draw!");
+    return "draw inside";
+  } else {
+    //   computer wins in all other cases
+    // console.log("computer wins");
+    return "comp";
+  }
+};
+
+
+const playGame = () => {
+  let userScore = 0;
+  let compScore = 0;
+  for (let i = 0; i < 5; i++) {
+    let userChoice = prompt("Enter your choice").toLowerCase();
+    console.log('user choice is: ' ,userChoice );
+    let computerChoice = computerPlay();
+    console.log('compueter choice is: ' ,computerChoice );
+    if (whoWon(userChoice, computerChoice) === "user") {
+      userScore += 10;
+    } else if (whoWon(userChoice, computerChoice) === "comp") {
+      compScore += 10;
+    }
+    console.log("userScore = ", userScore, "pc score: ", compScore);
+  }
+  if (userScore > compScore) {
+    console.log("User Won!!!");
+  } else if (compScore > userScore) {
+    console.log("Computer Won!!!");
+  } else {
+    console.log("draw!!!");
+  }
+};
+
+playGame();
