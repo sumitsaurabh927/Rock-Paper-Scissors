75a41b714fae9de4bf1160702fdeb293da2e7841
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-04-11 13:29:12 +0530

    game working with UI

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 // console.log('wired');
 
+// score handler wiring
+let userPoint = document.querySelector(".userScore");
+let computerPoint = document.querySelector(".computerScore");
+
+// round update wiring
+let infoStatus = document.querySelector(".infoStatus");
+let infoRound = document.querySelector(".infoRound");
+
+// choice update after each round
+let userPicked = document.querySelector(".userChoice");
+let compPicked = document.querySelector(".computerChoice");
+
 // wiring buttons
 let rockBtn = document.querySelector("#rock");
 let paperBtn = document.querySelector("#paper");
@@ -22,31 +34,58 @@ const whoWon = (userPlay, computerSelection) => {
   ) {
     console.log("User WINS!!!!");
     userScore += 10;
+    userPoint.innerText = `Player: ${userScore}`;
+    infoStatus.innerText = "Player Won!!!";
     console.log(userScore, compScore);
+    infoRound.innerText = `Rounds remaining: ${4 - count}`;
     count++;
     return "user";
   } else if (userPlay === computerSelection) {
     //   draw conditions
     console.log("draw!");
+    infoStatus.innerText = "Draw!!!";
     console.log(userScore, compScore);
+    infoRound.innerText = `Rounds remaining: ${4 - count}`;
     count++;
     return "draw inside";
   } else {
     //   computer wins in all other cases
     console.log("computer wins");
+    infoRound.innerText = `Rounds remaining: ${4 - count}`;
     count++;
     compScore += 10;
+    computerPoint.innerText = `Computer: ${compScore}`;
+    infoStatus.innerText = "Computer Won!!!";
     console.log(userScore, compScore);
     return "comp";
   }
 };
 
 const playRound = (userChoice) => {
+  // updating user choice on page
+  if (userChoice === "rock") {
+    userPicked.innerHTML = `Player picked:&#9994; `;
+  } else if (userChoice === "paper") {
+    userPicked.innerHTML = `Player picked:&#9995; `;
+  } else if (userChoice === "scissors") {
+    userPicked.innerHTML = `Player picked: &#9996; `;
+  }
+
   console.log(`User's choice is: ${userChoice}`);
 
   // computer's selection function
   let choicesArr = ["rock", "paper", "scissors"];
   let computerChoice = choicesArr[Math.floor(Math.random() * 3)];
+
+  // updating computer choice on page
+  if (computerChoice === "rock") {
+    compPicked.innerHTML = `Computer picked:&#9994; `;
+  } else if (computerChoice === "paper") {
+    compPicked.innerHTML = `Computer picked:&#9995; `;
+  } else if (computerChoice === "scissors") {
+    compPicked.innerHTML = `Computer picked: &#9996; `;
+  }
+
   console.log(`Comp's choice is ${computerChoice}`);
 
   // round result
@@ -56,10 +95,13 @@ const playRound = (userChoice) => {
   if (count === 5) {
     if (userScore > compScore) {
       console.log("User Won THE ENTIRE GAME!!!");
+      infoRound.innerText = "PLAYER Won THE ENTIRE GAME!!!";
     } else if (compScore > userScore) {
       console.log("Computer Won THE ENTIRE GAME!!!");
+      infoRound.innerText = "Computer Won THE ENTIRE GAME!!!";
     } else {
       console.log("THE ENTIRE GAME WAS draw!!!");
+      infoRound.innerText = "THE ENTIRE GAME WAS draw!!!";
     }
   }
 };
